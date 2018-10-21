930876b28ddaa6064c85e29756dfaa3c3f858470
David Floyd <davidfloyd91@gmail.com>
2018-10-21 23:26:38 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+];
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+};
+
+function addElementToBeginningOfArray(array,element) {
+  return [element, ...array];
+};
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+};
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element);
+};
+
+function accessElementInArray(array, element) {
+  return array[2];
+};
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+};
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+};
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+};
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
+};
